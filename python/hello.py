99a943305b6b25909848e14ce07a08dc8bc9d205
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-07-23 15:44:37 +0530

    initial commit

---
+++ python/hello.py
@@ -0,0 +1,26 @@
+x = 5
+y = 10
+
+print(x)
+print(y)
+
+#Single line comment
+
+'''
+Data Types:
+- int: numbers
+- float: decimal numbers
+- string: everything in between inverted commas
+- boolean: True/False
+'''
+
+x = "Hello"
+y = 'How are you?'
+
+print(x, y)
+print(x, y, sep="\t", end="")
+
+#By default, input stores values in string format
+name = input("Enter your name: ")
+marks = int(input("Enter your marks: "))
+percentage = float(input("Enter your percentage: "))
