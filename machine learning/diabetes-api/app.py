a9fe6bd9a7e5a0313aca5839a583782a430f3038
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-09-04 13:48:44 +0530

    api

--- machine learning/diabetes-api/app.py
+++ machine learning/diabetes-api/app.py
@@ -6,7 +6,28 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 loadedModel = pickle.load(open('KNN Model.pkl', 'rb'))
 
+#www.google.co.in/prediction
+
 #Routes
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/prediction', methods=['POST'])
+def prediction():
+    glucose = int(request.form['glucose'])
+    bmi = int(request.form['bmi'])
+    age = int(request.form['age'])
+
+    prediction = loadedModel.predict([[glucose, bmi, age]])[0]
+
+    if prediction == 0:
+        prediction = "Not Diabetic"
+    else:
+        prediction = "Diabetic"
+
+    return render_template('home.html', output=prediction)
 
 #Main function
 if __name__ == '__main__':
